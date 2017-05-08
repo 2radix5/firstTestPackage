ff2b14c0c3c5c6208d1cc10d09f26bee9857e5df
KEUNHO LEE <KEUNHO LEE>
2017-05-08 19:40:40 +0900

    This is second commit test

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,7 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
+# This is an example function named 'hello'
+# which prints 'Hello, git world!'.
 #
 # You can learn more about package authoring with RStudio at:
 #
@@ -14,5 +14,5 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  print("Hello, world!")
+  print("Hello, git world!")
 }
